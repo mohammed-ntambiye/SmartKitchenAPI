59118da12dd18cc34f3510ac8b90d275f88e59b6
Mohammed <mohammedntambi7@gmail.com>
2019-01-14 13:00:24 +0000

    first commit

---
+++ SmartKitchenApi/Models/SmartKitchenModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartKitchenApi.Models
+{
+    public class SmartKitchenModel
+    {
+        public string OrderId { get; set; }
+
+        public string StationId { get; set; }
+
+        public DateTime TimeScanned { get; set; }
+    }
+}
